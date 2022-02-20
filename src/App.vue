d172cc81c5f44a0c75adde8a7867667d7fc0b8d4
tzhiy <2570986081@qq.com>
2022-02-20 23:34:49 +0800

    feat: 接入 vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,37 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser" />
-    <!-- <ColumnList :list="list" /> -->
-    <ValidateForm @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <ValidateInput
-          :rules="emailRules"
-          v-model="emailVal"
-          type="text"
-          placeholder="请输入邮箱地址"
-          ref="inputRef"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <ValidateInput
-          :rules="passwordRules"
-          v-model="passwordVal"
-          type="password"
-          placeholder="请输入密码"
-          ref="inputRef"
-        />
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">提交</span>
-      </template>
-    </ValidateForm>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">© 2022 者也专栏</li>
+          <li class="list-inline-item">课程</li>
+          <li class="list-inline-item">文档</li>
+          <li class="list-inline-item">联系</li>
+          <li class="list-inline-item">更多</li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from 'vue'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const currentUser: UserProps = {
-  isLogin: true,
+  isLogin: false,
   name: 'viking'
 }
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description:
-      '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧 这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar:
-    //   'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar:
-      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
 export default defineComponent({
   name: 'App',
   components: {
-    // ColumnList,
-    GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalHeader
   },
   setup() {
-    const inputRef = ref<any>()
-    const emailVal = ref('')
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    const passwordVal = ref('')
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' }
-    ]
-    const onFormSubmit = (result: boolean) => {
-      console.log(result)
-    }
     return {
-      list: testData,
-      currentUser,
-      emailRules,
-      emailVal,
-      passwordVal,
-      passwordRules,
-      onFormSubmit,
-      inputRef
+      currentUser
     }
   }
 })
