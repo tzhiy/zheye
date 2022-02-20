02885863b47726ead1204d7201b58d06bb8fcc7f
tzhiy <2570986081@qq.com>
2022-02-21 00:06:23 +0800

    update: 接入 vuex

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import App from './App.vue'
+
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add(state) {
+      state.count++
+    }
+  }
+})
+
+console.log('count', store.state.count)
+store.commit('add')
+console.log('count', store.state.count)
 
 const routerHistory = createWebHistory()
 const router = createRouter({
@@ -26,6 +42,7 @@ const router = createRouter({
     }
   ]
 })
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
