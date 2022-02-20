693e4d58886a2c437afc605f807cfcf72962f15e
tzhiy <2570986081@qq.com>
2022-02-21 02:28:54 +0800

    update: 路由守卫根据状态跳转相应界面

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import CreatePost from './views/CreatePost.vue'
+import store from './store'
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
@@ -15,7 +16,8 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: { redirectAlreadyLogin: true }
     },
     {
       path: '/column/:id',
@@ -25,9 +27,19 @@ const router = createRouter({
     {
       path: '/create',
       name: 'create',
-      component: CreatePost
+      component: CreatePost,
+      meta: { requireLogin: true }
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireLogin && !store.state.user.isLogin) {
+    next({ name: 'login' })
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next({ name: 'home' })
+  } else {
+    next()
+  }
 })
 
 export default router
