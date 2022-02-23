797bd0b7db7f2ef42237f3a713d4206c93996091
tzhiy <2570986081@qq.com>
2022-02-23 14:29:53 +0800

    update: 转换数据结构

--- src/helper.ts
+++ src/helper.ts
@@ -41,3 +41,34 @@ export function beforeUploadCheck(file: File, condition: CheckCondition) {
     error
   }
 }
+
+interface TestProps {
+  _id: string;
+  name: string;
+}
+
+// 约束泛型，参数必须包含 _id?
+export const arrToObj = <T extends { _id?: string }>(arr: Array<T>) => {
+  return arr.reduce(
+    (prev, current) => {
+      if (current._id) {
+        prev[current._id] = current
+      }
+      return prev
+    },
+    {} as { [key: string]: T } // 类型断言
+  )
+}
+const testData: TestProps[] = [{ _id: '1', name: 'a' }, { _id: '2', name: 'b' }]
+const result = arrToObj(testData)
+console.log(result)
+
+export const objToArr = <T>(obj: { [key: string]: T }) => {
+  return Object.keys(obj).map(key => obj[key])
+}
+const testData2: { [key: string]: TestProps } = {
+  1: { _id: '1', name: 'a' },
+  2: { _id: '2', name: 'b' }
+}
+const result2 = objToArr(testData2)
+console.log(result2)
